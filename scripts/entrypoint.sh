aed16344dfca2fa23654f1faf2bb0869a4b8d962
mflynn-lanl <mflynn@lanl.gov>
2019-09-26 17:08:48 -0600

    updated data type, entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,6 +17,61 @@ elif [ "${1}" = "async" ] ; then
   sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
+  mkdir -p /data/kraken2
+  cd /data/kraken2
+
+  echo "downloading ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/minikraken2_v1_8GB_201904_UPDATE.tgz"
+  mkdir -p /data/kraken2
+  cd /data/kraken2
+
+  if [ -s "/data/kraken2/minikraken2_v1_8GB/database100mers.kmer_distrib" ];
+  then
+    echo "minikraken2_v1_8GB exists"
+  else
+    wget -q ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/minikraken2_v1_8GB_201904_UPDATE.tgz
+    tar -xzvf minikraken2_v1_8GB_201904_UPDATE.tgz
+    rm minikraken2_v1_8GB_201904_UPDATE.tgz
+  fi
+
+  echo "downloading ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/16S_Greengenes_20190418.tgz"
+
+  if [ -s "/data/kraken2/16S_Greengenes_20190418/database100mers.kmer_distrib" ];
+  then
+    echo "16S_Greengenes_20190418 exists"
+  else
+    wget -q ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/16S_Greengenes_20190418.tgz
+    tar -xzvf 16S_Greengenes_20190418.tgz
+    rm 16S_Greengenes_20190418.tgz
+  fi
+
+  echo "downloading ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/16S_RDP_20190418.tgz"
+  if [ -s "/data/kraken2/16S_RDP_20190418/database100mers.kmer_distrib" ];
+  then
+    echo "16S_RDP_20190418.tgz exists"
+  else
+    wget -q ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/16S_RDP_20190418.tgz
+    tar -xzvf 16S_RDP_20190418.tgz
+    rm 16S_RDP_20190418.tgz
+  fi
+
+  echo "downloading ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/16S_Silva_20190418.tgz"
+  if [ -s "/data/kraken2/16S_Silva_20190418/database200mers.kmer_distrib" ];
+  then
+    echo "16S_Silva_20190418 exists"
+  else
+    wget -q ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/16S_Silva_20190418.tgz
+    tar -xzvf 16S_Silva_20190418.tgz
+    rm 16S_Silva_20190418.tgz
+  fi
+
+  if [ -s "/data/kraken2/minikraken2_v1_8GB/database100mers.kmer_distrib" -a -s "/data/kraken2/16S_Greengenes_20190418/database100mers.kmer_distrib" -a -s "/data/kraken2/16S_RDP_20190418/database100mers.kmer_distrib" -a -s "/data/kraken2/16S_Silva_20190418/database200mers.kmer_distrib" ] ; then
+    echo "DATA DOWNLOADED SUCCESSFULLY"
+    touch /data/__READY__
+  else
+    echo "Init failed"
+  fi
+
+
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
